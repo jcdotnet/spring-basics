3613bf03334101c8d56a0e7353f4fd319eda0612
JC <jcdotnet@hotmail.com>
2025-08-03 21:05:04 +0200

    Spring Boot :: Injecting App Properties

--- spring-boot/src/main/java/com/josecarlosroman/basics/spring/boot/demo/rest/HelloRestController.java
+++ spring-boot/src/main/java/com/josecarlosroman/basics/spring/boot/demo/rest/HelloRestController.java
@@ -1,20 +1,29 @@
 package com.josecarlosroman.basics.spring.boot.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
 
+    @Value("${hello.message}")
+    private String helloMessage;
+
+    @Value("${beautiful.message}")
+    private String beautifulMessage;
+
     // expose "/" endpoint that returns "Hello World"
     @GetMapping("/")
     public String SayHello() {
-        return "Hello World";
+        // return "Hello World";
+        return helloMessage;
     }
 
     // expose "/beautiful" endpoint
     @GetMapping("/beautiful")
     public String SayBeautiful() {
-        return "Hello, Beautiful World!";
+        // return "Hello, Beautiful World!";
+        return beautifulMessage;
     }
 }
