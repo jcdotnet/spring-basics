1b69f79854831c03fba858b660db1ab5e296df6c
JC <jcdotnet@hotmail.com>
2025-08-14 16:00:52 +0200

    Added Spring Boot & Tailwind CSS project

---
+++ spring-boot-tailwindcss/src/main/java/com/josecarlosroman/com/basics/spring/boot/tailwindcss/SpringBootTailwindApplication.java
@@ -0,0 +1,13 @@
+package com.josecarlosroman.com.basics.spring.boot.tailwindcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTailwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTailwindApplication.class, args);
+	}
+
+}
---
+++ spring-boot-tailwindcss/src/test/java/com/josecarlosroman/com/basics/spring/boot/tailwindcss/SpringBootTailwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josecarlosroman.com.basics.spring.boot.tailwindcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTailwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
