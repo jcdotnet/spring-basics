ca9474f8b90aae3d4351a69bf06c24ce82dc06ae
JC <jcdotnet@hotmail.com>
2025-08-25 19:22:33 +0200

    Added GradeBook MVC project with existing code

---
+++ spring-boot-mvc-test/src/main/java/com/josecarlosroman/basics/spring/boot/mvc/gradebook/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.josecarlosroman.basics.spring.boot.mvc.gradebook;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GradebookApplication.class);
+	}
+
+}
---
+++ spring-boot-mvc-test/src/main/java/com/josecarlosroman/basics/spring/boot/mvc/gradebook/models/Student.java
@@ -0,0 +1,9 @@
+package com.josecarlosroman.basics.spring.boot.mvc.gradebook.models;
+
+public interface Student {
+
+   String studentInformation();
+
+   String getFullName();
+
+}
